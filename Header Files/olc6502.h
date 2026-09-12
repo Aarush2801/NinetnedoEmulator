5b4e87e8a3a6652e8085c0531c968801af893a99
Aarush2801 <a882sing@uwaterloo.ca>
2026-09-12 11:25:29 -0400

    this is driving me crazy

--- Header Files/olc6502.h
+++ Header Files/olc6502.h
@@ -13,7 +13,16 @@ class olc6502
         enum FLAGS6502
         {
             C = (1 << 0), //this is the carry bit apparently
-        }
+            Z = (1<<1), //Zero
+            I = (1<<2), //Disable Interrupts
+            D = (1<<3),// Decimal Mode (Not gonna implement this rn cuz the original nes didn't have it either)
+            B = (1<<4),//Break
+            U = (1<<5),//Unused
+            V = (1<<6),//Overflow
+            N = (1<<7),//Negative
+
+
+        };
         
 
 
